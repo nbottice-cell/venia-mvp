d2c41e4130442680e6310be260e47f9f01a0b33b
nbottice-cell <nbottice@gmail.com>
2026-04-13 19:16:40 -0400

    Fix Vercel build errors

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Allow production builds even if there are type errors
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Allow production builds even if there are ESLint errors
     ignoreDuringBuilds: true,
   },
 }
